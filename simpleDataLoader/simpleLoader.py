e14282c6b6fa6152cbfba1b8688f2e63f668ae29
dmytrochornyy <72020980+dmytrochornyy@users.noreply.github.com>
2021-02-21 03:06:15 +0200

    number update

--- simpleDataLoader/simpleLoader.py
+++ simpleDataLoader/simpleLoader.py
@@ -1,36 +1,41 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.decomposition import PCA
+from sklearn.preprocessing import OneHotEncoder
 
 def number_data(dataframe):
     x = dataframe[['book_pages', 'book_review_count', 'book_rating_count', 'book_rating']]
-    x.book_pages = x.book_pages.apply(lambda x: str(x).split()[0])
-    x.book_pages = x.book_pages.apply(lambda y: -1 if y == "nan" else int(y))
-    x.book_pages[x.book_pages == -1] = x.book_pages[x.book_pages != -1].mean() 
+    x = x.dropna()
+    x.book_pages = x.book_pages.apply(lambda x: int(str(x).split()[0]))
     
-    x = x.fillna(x.mean())
-
     feature_to_cut = ['book_pages', 'book_review_count', 'book_rating_count']
-
+    lower = {}
+    upper = {}
     for feature in feature_to_cut:
-
-        x = x[(x[feature] > x[feature].quantile(0.1)) & (x[feature] < x[feature].quantile(0.999))]
+        lower[feature] = x[feature].quantile(0.01)
+        upper[feature] = x[feature].quantile(0.999)
+        x = x[(x[feature] > x[feature].quantile(0.01)) & (x[feature] < x[feature].quantile(0.999))]
 
 
     y = x.book_rating
     x = x.drop(axis=1, labels=['book_rating'])
 
-    scaler = MinMaxScaler()
-    data = pd.DataFrame(scaler.fit_transform(x))
-    data = data.rename(columns={0:'book_pages',1:'book_review_count',2:'book_rating_count'})
+    return x, y, lower, upper
 
-    decompostor = PCA(1)
-    x = pd.concat([pd.DataFrame(x.book_pages).reset_index().drop(['index'], 1), pd.DataFrame(decompostor.fit_transform(data[['book_review_count', 'book_rating_count']])).reset_index().drop(['index'], 1)], 1)
-    x = x.rename(columns={0:"popularity"})
-    return x, y
 
+def number_data_test(dataframe, lower, upper):
+    x = dataframe[['book_pages', 'book_review_count', 'book_rating_count']]
+    x = x.fillna(0)
+    x.book_pages = x.book_pages.apply(lambda x: int(str(x).split()[0]))
+    
+
+    feature_to_cut = ['book_pages', 'book_review_count', 'book_rating_count']
+
+    for feature in feature_to_cut:
+        x[x[feature] < lower[feature]][feature] = lower[feature]
+        x[x[feature] > upper[feature]][feature] = upper[feature]
+
+    return x
 
 def book_format_data(dataframe):
     x = dataframe['book_format']
